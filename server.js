0f711fa106c5448afd8a060fdacd52f404de0565
Patrick Adonteng <pat64j@live.com>
2020-08-13 18:30:26 +0000

    update production server file

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
+
+
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
